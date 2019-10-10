db0ccfcaf70eb1675499fe7db2f9a538187cb4be
Richard J. Tobias <rjjt@cephasonics.com>
2019-10-10 13:34:19 +0100

    added some routing

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './homepage.styles.scss'
 
-import Directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component.jsx'
 
 const HomePage = () => (
     <div classNmae='homepage'>
