eb39829ec07a715b631969279941f95835c4c347
Richard J. Tobias <rjjt@cephasonics.com>
2019-10-12 22:17:17 -0700

    sign up working, but email sign in is not

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 import './sign-in.styles.scss'
 
@@ -16,10 +16,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault()
 
-        this.setState({email: '', password: ''})
+        const { email, password } = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email: '', password: ''})
+        }
+        catch (error) {
+            console.error(error)
+        }
     }
 
     handleChange = event => {
@@ -40,18 +47,18 @@ class SignIn extends React.Component {
                         type='email' 
                         value={this.state.email} 
                         handleChange={this.handleChange}
-                        label='email'
+                        label='Email'
                         required 
                     />
                     <FormInput 
                         name='password' 
                         type='password' 
-                        value={this.state.email} 
+                        value={this.state.password} 
                         handleInput={this.handleChange}
-                        label='password'
+                        label='Password'
                         required 
                     />
-                    <div className='button'>
+                    <div className='buttons'>
                     <CustomButton type='submit'>Sign In</CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                         {' '}
