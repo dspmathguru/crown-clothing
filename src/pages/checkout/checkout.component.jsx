f6928d379a130aaba37a901a35f40c5ef92af434
Richard J. Tobias <rjjt@cephasonics.com>
2019-10-17 12:48:12 -0700

    added ability to remove an item

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { createStructuredSelector } from 'reselect'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.components'
+
 import './checkout.styles.scss'
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -24,7 +26,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     {
       cartItems.map(cartItem =>
-        cartItem.name
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       )
     }
     <div className='total'>
